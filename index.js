d6442283c6e500f91c1b20e53b8bfd6e96359321
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2022-01-11 14:51:16 +0000

    Add Jest plugin to ESLint (#18)
    * Add Jest plugin
    
    * Remove duplicate rule
    
    * Add Jest rule for consistent `describe` at top level
    
    * Move to `jest/all` ruleset
    
    Includes `recommended` + `style` sets
    
    * Disable `jest/no-hooks` in the `jest/style` ruleset
    
    * I'm an idiot - remove `jest/all` includes more rules that I realised
    
    * 2.4.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Typescript parser for ESLint
+  // Typescript parser for ESLint
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
     project: './tsconfig.json',
@@ -11,35 +12,72 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
     'import',
-    'security'
+    'security',
+    'jest'
   ],
   extends: [
-    'airbnb-typescript/base', // Airbnb style guide rules
-    'plugin:@typescript-eslint/recommended', // ESLint Typescript plugin recommended rules
-    'plugin:@typescript-eslint/recommended-requiring-type-checking', // Additional type-checking rules
-    'plugin:import/typescript', // ESLint Typescript Import plugin
-    'plugin:security/recommended' // Security rules
+    // Airbnb style guide rules
+    'airbnb-typescript/base',
+    // ESLint Typescript plugin recommended rules
+    'plugin:@typescript-eslint/recommended',
+    // Additional type-checking rules
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // ESLint Typescript Import plugin
+    'plugin:import/typescript',
+    // Security rules
+    'plugin:security/recommended',
+    // Jest rules
+    'plugin:jest/recommended',
+    'plugin:jest/style'
+  ],
+  overrides: [
+    // @see https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
+    {
+      // Only turn the original rule off for test files.
+      'files': ['tests/**', 'test/**'],
+      'plugins': ['jest'],
+      'rules': {
+        '@typescript-eslint/unbound-method': 'off',
+        'jest/unbound-method': 'error'
+      }
+    }
   ],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off', // Interfaces don't have to start with a I
-    '@typescript-eslint/no-use-before-define': 'off', // Allow defining functions (incl. arrow expressions) after use as per 'Stepdown Rule' best practice
+    // Interfaces don't have to start with a I
+    '@typescript-eslint/interface-name-prefix': 'off',
+    // Allow defining functions (incl. arrow expressions) after use as per 'Stepdown Rule' best practice
+    '@typescript-eslint/no-use-before-define': 'off',
+    // Allow referencing unbound methods as long as they are static
     '@typescript-eslint/unbound-method': [
-      'error', 
+      'error',
       { 'ignoreStatic': true }
-    ], // Allow referencing unbound methods as long as they are static
-    'class-methods-use-this': 'off', // Stops us from having to declare class methods which don't use this as static.
-    'eol-last': 2, // Enforces that files end with a newline 
-    'import/no-unresolved': [ // Allows you to use imports which can't be resolved, enabled for everything not in the ignore list.
-      2,
-      { 'ignore': ['@azure/functions'] } 
     ],
-    'import/prefer-default-export': 'off', // Allow named exports where there is only one module export
-    'max-len': 'off', // Disable default max line length of 100
-    'no-console': 'off', // Allow console logging for dev/test purposes
-    'no-plusplus': 'off', // Allow use of ++/-- operators given issues are extremely edge case
+    // Stops us from having to declare class methods which don't use this as static.
+    'class-methods-use-this': 'off',
+    // Enforces that files end with a newlines
+    'eol-last': 2,
+    // Allows you to use imports which can't be resolved, enabled for everything not in the ignore list.
+    'import/no-unresolved': [
+      2,
+      { 'ignore': ['@azure/functions'] }
+    ],
+    // Allow named exports where there is only one module export
+    'import/prefer-default-export': 'off',
+    // Disable default max line length of 100
+    'max-len': 'off',
+    // Allow console logging for dev/test purposes
+    'no-console': 'off',
+    // Allow use of ++/-- operators given issues are extremely edge case
+    'no-plusplus': 'off',
+    // Allow reassigning parameter properties but not whole parameters
     'no-param-reassign': [
-      'error', 
+      'error',
       { 'props': false }
-    ] // Allow reassigning parameter properties but not whole parameters
+    ],
+    // @see https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/require-top-level-describe.md
+    'jest/require-top-level-describe': 'error',
+    // @see https://github.com/jest-community/eslint-plugin-jest/blob/v25.3.0/docs/rules/no-hooks.md
+    // Enable use of hooks (beforeEach/afterEach) - state is useful in most testsuites
+    'jest/no-hooks': 'off',
   },
 }
